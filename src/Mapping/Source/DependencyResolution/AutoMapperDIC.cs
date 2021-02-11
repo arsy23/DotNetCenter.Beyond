9ac327725803ae9c6d429bce820676ec188df6c8
arsalan falahpour <arsalan.falahpour@outlook.com>
2021-02-11 19:45:38 +0330

    Create initial project 'DotNetCenter.Beyond.Mapping'

---
+++ src/Mapping/Source/DependencyResolution/AutoMapperDIC.cs
@@ -0,0 +1,53 @@
+﻿namespace DotNetCenter.Beyond.Mapping.DependencyResolution
+{
+    using DotNetCenter.DateTime.DependencyResolution;
+    using global::AutoMapper;
+    using Microsoft.Extensions.DependencyInjection;
+    using System.Collections.Generic;
+    using System.Reflection;
+
+    /// <summary>
+    /// AutoMapper Dependency Injection Container
+    /// </summary>
+    public static class AutoMapperDIC
+    { 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="assembly"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddDefaultAutoMapperServices(this IServiceCollection services, Assembly assembly)
+        {
+
+            var assemblies = new List<Assembly>();
+            assemblies?.AddDotNetCenterDateTimeAssembly();
+            assemblies?.Add(assembly);
+            if (assemblies is not null)
+            services.AddAutoMapper(assemblies);
+            return services;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="assemblies"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddDefaultAutoMapperServices(this IServiceCollection services, List<Assembly> assemblies)
+        {
+
+            assemblies?.AddDotNetCenterDateTimeAssembly();
+            if (assemblies is not null)
+                services.AddAutoMapper(assemblies);
+            return services;
+        }
+        private static List<Assembly>? AddDotNetCenterDateTimeAssembly(this List<Assembly> assemblies)
+        {
+            var dateTimeAssembly = Assembly.GetAssembly(typeof(DateTimeDIC));
+            if (dateTimeAssembly is not null)
+                assemblies?.Add(dateTimeAssembly);
+            return assemblies;
+        }
+
+    }
+}
---
+++ src/Mapping/Source/MappingExtension.cs
@@ -0,0 +1,47 @@
+﻿namespace DotNetCenter.Beyond.Mapping
+{
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using Microsoft.EntityFrameworkCore;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class MapperExtension
+    {
+        /// <summary>
+        /// Projection from queryable model
+        /// </summary>
+        /// <typeparam name="TDestination"></typeparam>
+        /// <param name="queryable">Queryable Model</param>
+        /// <param name="mapper">AutoMapper Mapper from DotNetCenter.Byond.A</param>
+        /// <returns></returns>
+        public static async Task<List<TDestination>> ProjectToListAsync<TDestination>(this IQueryable queryable, IMapper mapper) 
+            => await queryable
+            .ProjectTo<TDestination>(mapper.ConfigurationProvider)
+            .ToListAsync();
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TDestination"></typeparam>
+        /// <param name="queryable"></param>
+        /// <param name="mapper"></param>
+        /// <returns></returns>
+        public static IQueryable<TDestination> ProjectToQueryable<TDestination>(this IQueryable queryable, IMapper mapper) 
+            => queryable
+            .ProjectTo<TDestination>(mapper.ConfigurationProvider);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TDestination"></typeparam>
+        /// <param name="queryable"></param>
+        /// <param name="mapper"></param>
+        /// <returns></returns>
+        public static async Task<TDestination> ProjectToSingleOrDefaultAsync<TDestination>(this IQueryable queryable, IMapper mapper) 
+            => await queryable
+            .ProjectTo<TDestination>(mapper.ConfigurationProvider)
+            .SingleOrDefaultAsync();
+    }
+}
