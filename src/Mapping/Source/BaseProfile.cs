45493f7bf310f5449c71531ff85ccd9efebd4a5c
arsalan falahpour <arsalan.falahpour@outlook.com>
2021-02-11 22:34:44 +0330

    Refactoring 'BaseProfile' class

--- src/Mapping/Source/BaseProfile.cs
+++ src/Mapping/Source/BaseProfile.cs
@@ -7,84 +7,122 @@
     using System.Reflection;
     public class BaseProfile : Profile
     {
-        // For custom mapping and when this assembly scaned this base profile confused with this base class with other mapping profiles in this assembly
-        //This code show exception for imapto if the objects inheriet from a generic type and that generic type again inheriet as parent of another type that inheriet to object
-        //Like -> Example : BaseClass, IAuditable, IMapTo<SomeEntity>
-        //BaseClass : IBaseEntity<Guid>
-        //IAuditale : IBaseEntity<Guid>
+        #region Constructors
         public BaseProfile()
         { }
         public BaseProfile(Assembly executingAssembly) 
             => ApplyMappingsFromAssembly(executingAssembly);
+        #endregion
         private void ApplyMappingsFromAssembly(Assembly assembly)
         {
-             _ = InvokeIMapFrom(assembly);
+            InvokeIMapFrom(assembly);
             InvokeIMapTo(assembly);
         }
-
-        private void InvokeIMapTo(Assembly assembly)
+        #region Invoke Methods
+        private bool InvokeIMapTo(Assembly assembly)
         {
-            foreach (var type in GetIMapToExportedTypes(assembly))
-                    InvokeMappingMethodImplementationForIMapTo(type);
+            var isAllInvoked  = true;
+            var exportedTypes = GetIMapToExportedTypes(assembly);
+            if (exportedTypes is null)
+                isAllInvoked = false;
+            else
+                foreach (var type in exportedTypes)
+                    isAllInvoked &= InvokeMappingMethodImplementationForIMapTo(type);
+
+            return isAllInvoked;
         }
         private bool InvokeIMapFrom(Assembly assembly)
         {
-            var invoked = false;
-            foreach (var type in GetIMapFromExportedTypes(assembly))
-            {
-                invoked = InvokeMappingMethodForIMapFrom(type);
-                if (invoked) 
-                return true;
-            }
-            return invoked;
+            var isAllInvoked = true;
+            var exportedTypes = GetIMapFromExportedTypes(assembly);
+            if (exportedTypes is null)
+                return false;
+            else
+                foreach (var type in exportedTypes)
+                    isAllInvoked &= InvokeMappingMethodForIMapFrom(type);
+            return isAllInvoked;
         }
         private bool InvokeMappingMethodForIMapFrom(Type type)
         {
             var instance = Activator.CreateInstance(type);
-            var invoked = false;
-            if (!TryInvokeClassMethodImplementation(type, instance))
-                invoked = TryInvokeInterfacMethodImplementation(GetMapFromInterfaces(type), instance);
-            else if(TryInvokeClassMethodImplementation(type, instance))
+            bool isAllInvoked;
+            if (instance is null)
+                return false;
+            if (TryInvokeClassMethodImplementation(type, instance))
                 return true;
-            return invoked;
+            else 
+                isAllInvoked = TryInvokeInterfacMethodImplementation(GetMapFromInterfaces(type), instance);
+
+            return isAllInvoked;
         }
-        private void InvokeMappingMethodImplementationForIMapTo(Type type)
+        private bool InvokeMappingMethodImplementationForIMapTo(Type type)
         {
             var instance = Activator.CreateInstance(type);
+            if (instance == null)
+                return false;
             if (!TryInvokeClassMethodImplementation(type, instance))
-                TryInvokeInterfacMethodImplementation(GetMapToInterfaces(type), instance);
+                return TryInvokeInterfacMethodImplementation(GetMapToInterfaces(type), instance);
+            return true;
         }
+        #endregion
+        #region TryToInvoke Methods
         private bool TryInvokeClassMethodImplementation(Type type, object instance)
         {
             var methodInfo = type?.GetMethod("Mapping");
             if (methodInfo is null)
                 return false;
-            methodInfo.Invoke(instance, new object[] { this });
+            methodInfo?.Invoke(instance, new object[] { this });
             return true;
         }
-
-        private static IEnumerable<Type> GetMapToInterfaces(Type type) => type.GetInterfaces().Where(o => o.IsGenericType && o.GetGenericTypeDefinition() == typeof(IMapTo<>));
-        private static List<Type> GetMapFromInterfaces(Type type) => type.GetInterfaces().Where(o => o.GetGenericTypeDefinition() == typeof(IMapFrom<>) && o.IsGenericType).ToList();
-        private static List<Type> GetIMapFromExportedTypes(Assembly assembly) => assembly.GetExportedTypes()
-            .Where(t => t.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IMapFrom<>)))
-            .ToList();
-        private static List<Type> GetIMapToExportedTypes(Assembly assembly) => assembly.GetExportedTypes()
-            .Where(t => t.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IMapTo<>)))
-            .ToList();
-
         private bool TryInvokeInterfacMethodImplementation(IEnumerable<Type> types, object instance)
         {
             foreach (var @interface in types)
             {
-                //@interface.GetMethod("Mapping")?.Invoke(instance, new object[] { this });
                 var mapping = @interface?.GetMethod("Mapping");
-                mapping?.Invoke(instance, new object[] { this });
-                if (!(mapping is null))
+                if (mapping is null)
+                    continue;
+                else
+                {
+                    mapping?.Invoke(instance, new object[] { this });
                     return true;
+                }
             }
             return false;
         }
-
+        #endregion
+        #region GetMap Interfaces
+        private static IEnumerable<Type> GetMapToInterfaces(Type type)
+            => type
+            .GetInterfaces()
+            .Where(o => 
+            o.IsGenericType 
+            && o.GetGenericTypeDefinition() == typeof(IMapTo<>));
+        private static List<Type> GetMapFromInterfaces(Type type) 
+            => type
+            .GetInterfaces()
+            .Where(o => 
+            o.GetGenericTypeDefinition() == typeof(IMapFrom<>) 
+            && o.IsGenericType)
+            .ToList();
+        #endregion
+        #region GetMap ExportedTypes
+        private static List<Type> GetIMapFromExportedTypes(Assembly assembly) 
+            => assembly
+            .GetExportedTypes()
+            .Where(t => 
+            t.GetInterfaces()
+            .Any(i => 
+            i.IsGenericType 
+            && i.GetGenericTypeDefinition() == typeof(IMapFrom<>)))
+            .ToList();
+        private static List<Type> GetIMapToExportedTypes(Assembly assembly) 
+            => assembly
+            .GetExportedTypes()
+            .Where(t => 
+            t.GetInterfaces()
+            .Any(i => i.IsGenericType 
+            && i.GetGenericTypeDefinition() == typeof(IMapTo<>)))
+            .ToList();
+        #endregion
     }
-
 }
