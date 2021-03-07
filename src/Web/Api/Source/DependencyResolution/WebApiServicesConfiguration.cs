0df0144d1f15d178feb46aa2efb317e5914a7750
arsalan falahpour <arsalan.falahpour@outlook.com>
2021-03-08 00:25:45 +0330

    Stage Web.Api to released on v0.1.0

---
+++ src/Web/Api/Source/DependencyResolution/WebApiServicesConfiguration.cs
@@ -0,0 +1,18 @@
+﻿namespace DotNetCenter.Beyond.Web.Api.DependencyResolution
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.DependencyInjection;
+    using System;
+    using DotNetCenter.Beyond.Web.Core.DependencyResolution;
+
+    public static class WebApiServicesConfiguration
+    {
+        public static IServiceCollection AddWebApiServices(this IServiceCollection services, Action<MvcOptions> options, bool lowercaseUrls)
+        {
+            services.AddCoreServices(lowercaseUrls);
+            var mvcBuilder = services.AddControllers(options);
+            services.AddSingleton(mvcBuilder);
+            return services;
+        }
+    }
+}
