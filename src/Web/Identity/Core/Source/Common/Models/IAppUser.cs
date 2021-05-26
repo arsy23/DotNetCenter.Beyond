29e1e624b93c6b20b408bfd5b9bb5e9148b5d197
arsalan falahpour <arsalan.falahpour@outlook.com>
2021-05-27 00:10:21 +0430

    Add Username and Basic-Email Fields

--- src/Web/Identity/Core/Source/Common/Models/IAppUser.cs
+++ src/Web/Identity/Core/Source/Common/Models/IAppUser.cs
@@ -4,6 +4,7 @@ namespace DotNetCenter.Beyond.Web.Identity.Core.Models
     using DotNetCenter.Core.Entities;
     using Microsoft.AspNetCore.Identity;
     using System;
+    using System.Collections.Generic;
 
     public interface IAppUser : AuditableEntity<Guid, Guid>
         , TrackableEntity
@@ -12,6 +13,8 @@ namespace DotNetCenter.Beyond.Web.Identity.Core.Models
         public string ProfilePictureAddress { get; }
         public string FirstName { get; }
         public string LastName { get; }
+        public string Username { get; }
+        public string Email { get; }
         public string DisplayName { get; }
         public string AccountType { get; }
         public System.DateTime DateRegistered { get; }
